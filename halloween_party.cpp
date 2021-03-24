cc38d3c9130e835f58f5642feae4a9140c6860fa
Suyash Ramteke <suyash.ramteke@gmail.com>
2021-03-23 23:34:04 -0400

    Create halloween_party.cpp

---
+++ halloween_party.cpp
@@ -0,0 +1,87 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the halloweenParty function below.
+ */
+long long int halloweenParty(long int k) {
+    /*
+     * Write your code here.
+     */
+     long long int ans;
+     if(k%2==0)
+        ans=k*k/4;
+    else
+        ans=(k-1)*(k+1)/4;
+
+        printf("%lld", ans);
+        return ans;
+
+
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* t_endptr;
+    char* t_str = readline();
+    int t = strtol(t_str, &t_endptr, 10);
+
+    if (t_endptr == t_str || *t_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        char* k_endptr;
+        char* k_str = readline();
+        int k = strtol(k_str, &k_endptr, 10);
+
+        if (k_endptr == k_str || *k_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        long result = halloweenParty(k);
+
+        fprintf(fptr, "%ld\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
